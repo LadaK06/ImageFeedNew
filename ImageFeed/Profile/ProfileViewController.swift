9489efd262b8e03dda2da4fad451f9b4a25075a7
DaniilSh23 <danyasevas11@gmail.com>
2024-08-03 19:52:00 +0300

    удален раздел избранное на экране профиля

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -28,8 +28,8 @@ class ProfileViewController: UIViewController {
         createNameLabel()
         createLoginLabel()
         createDescriptionLabel()
-        createFavoritesLabel()
-        createNoPhotoImageView()
+//        createFavoritesLabel()
+//        createNoPhotoImageView()
         createLogoutButton()
     }
     
